0f5ded3de8b2b9f66c5ef8e844bb135e9af80833
Lynjai Jimenez <139661024+IntermediaDesigns@users.noreply.github.com>
2023-12-20 15:26:37 -0500

    .

--- src/app/components/Posts.jsx
+++ src/app/components/Posts.jsx
@@ -1,42 +1,42 @@
-'use client';
-import { useState, useEffect } from 'react';
+// 'use client';
+// import { useState, useEffect } from 'react';
 
-export default function Posts() {
-  const [posts, setPosts] = useState([]);
+// export default function Posts() {
+//   const [posts, setPosts] = useState([]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch(`/api/posts`, {
-        method: "POST",
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          title: "",
-          message: "",
-          subredditId: "",
-        }),
-      });
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       const response = await fetch(`/api/posts`, {
+//         method: "POST",
+//         headers: {
+//           'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify({
+//           title: "",
+//           message: "",
+//           subredditId: "",
+//         }),
+//       });
 
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
+//       if (!response.ok) {
+//         throw new Error('Network response was not ok');
+//       }
 
-      const data = await response.json();
-      setPosts(data);
-    };
+//       const data = await response.json();
+//       setPosts(data);
+//     };
 
-    fetchData();
-  }, []);
+//     fetchData();
+//   }, []);
 
-  return (
-    <div>
-      {posts.map((post, index) => (
-        <div key={index}>
-          <h2>{post.title}</h2>
-          <p>{post.message}</p>
-        </div>
-      ))}
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       {posts.map((post, index) => (
+//         <div key={index}>
+//           <h2>{post.title}</h2>
+//           <p>{post.message}</p>
+//         </div>
+//       ))}
+//     </div>
+//   );
+// }
