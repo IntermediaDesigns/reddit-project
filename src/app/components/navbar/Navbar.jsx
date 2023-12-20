b8f9c8384c82826102dc2786c579e4df88bcc0bf
Lynjai Jimenez <139661024+IntermediaDesigns@users.noreply.github.com>
2023-12-19 21:34:53 -0500

    .

--- src/app/components/navbar/Navbar.jsx
+++ src/app/components/navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import styles from '@/app/page.module.css';
 
 export default function Navbar() {
   return (
-    <nav className='fixed w-full h-20 shadow-xl bg-white flex justify-between items-center px-4 top-0'>
+    <nav className='fixed w-full h-20 shadow-xl bg-white flex justify-between items-center pl-6 pr-6 top-0'>
       <div>
         <Link href={'/'}>
           <img
@@ -16,25 +16,35 @@ export default function Navbar() {
         </Link>
       </div>
 
-      <div className='flex items-center gap-6 justify-center text-center'>
-        <Link href={'/'}>
-          <img className=' text-center text-slate-900 text-lg ' src='/Home.png' alt='Home' width='25' height='25' />
+      <div className='flex items-center justify-center gap-6 '>
+        <Link className="active:border-b-tomato" href={'/'}>
+          <img src='/Home.png' alt='Home' width='30' />
         </Link>
 
-        <Link className='' href={'/subreddits'}>
+        <Link
+          className=' font-mina text-slate-900 hover:text-tomato active:text-tomato'
+          href={'/subreddits'}
+        >
           Subreddits
         </Link>
-        <Link className='' href={'/logout'}>
+        <Link
+          className=' font-mina text-slate-900 hover:text-tomato active:text-tomato'
+          href={'/logout'}
+        >
           Logout
         </Link>
       </div>
 
-      <div className=''>
+      <div className=' flex gap-2 '>
         <Link href='/login'>
-          <button className=''>Login</button>
+          <button className='flex justify-center items-center w-20 h-8 rounded-lg bg-light-blue-500 border-2 gradient-border text-slate-900 font-mina text-sm leading-5 no-underline hover:bg-radial-gradient cursor-pointer'>
+            Login
+          </button>
         </Link>
         <Link href='/register'>
-          <button className=''>Register</button>
+          <button className='flex justify-center items-center w-20 h-8 rounded-lg bg-light-blue-500 border-2 border-tomato text-slate-900 font-mina text-sm leading-5 no-underline hover:bg-radial-gradient cursor-pointer'>
+            Register
+          </button>
         </Link>
       </div>
     </nav>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,30 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    fontFamily: {
+      bungee: ['Bungee Spice', 'sans-serif'],
+      mina: ['Mina', 'sans-serif'],
+    },
     extend: {
+      gradients: theme => ({
+        'gradient-border': ['90deg', '#ff6347 0%', '#f3c971 100%'],
+      }),
+      
+      colors: {
+        tomato: '#ff6347',
+        redditblue: '#58c6fc',
+      },
+
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'radial-gradient':
+          'radial-gradient(circle at center, #58c6fc, #dbebf3)',
       },
     },
   },
-  plugins: [],
-}
+  plugins: [
+    require('tailwindcss-gradients'),
+  ],
+};
