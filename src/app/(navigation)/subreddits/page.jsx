09236a2b3b41c1c63371e11a76fa40ecf07de4b2
Lynjai Jimenez <139661024+IntermediaDesigns@users.noreply.github.com>
2023-12-20 08:28:21 -0500

    ..

--- src/app/(navigation)/subreddits/page.jsx
+++ src/app/(navigation)/subreddits/page.jsx
@@ -1,4 +1,10 @@
+import styles from '@/app/page.module.css';
+
 export default function Subreddits() {
-       return <div>Subreddits</div>;
+       return (
+       <div>
+        Subreddits
+        
+       </div>);
      }
      
