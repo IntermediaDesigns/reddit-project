ce09e7b33ad6fdc38afad8395c0d723caa117f12
Lynjai Jimenez <139661024+IntermediaDesigns@users.noreply.github.com>
2023-12-19 23:40:45 -0500

    navbar done

--- src/app/components/navbar/Navbar.jsx
+++ src/app/components/navbar/Navbar.jsx
@@ -1,52 +1,125 @@
+'use client';
 import Link from 'next/link';
-import styles from '@/app/page.module.css';
+import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
+import { useState } from 'react';
 
 export default function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleNav = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
-    <nav className='fixed w-full h-20 shadow-xl bg-white flex justify-between items-center pl-6 pr-6 top-0'>
-      <div>
-        <Link href={'/'}>
-          <img
-            className='cursor:pointer'
-            src='/Logo.png'
-            alt='Logo'
-            width='60'
-            height='55'
-          />
-        </Link>
-      </div>
+    <>
+      <nav className='fixed w-full h-20 shadow-xl bg-white flex justify-between items-center pl-6 pr-6 top-0 z-20'>
+        <div>
+          <Link href={'/'}>
+            <img
+              className='cursor:pointer'
+              src='/Logo.png'
+              alt='Logo'
+              width='60'
+              height='55'
+            />
+          </Link>
+        </div>
 
-      <div className='flex items-center justify-center gap-6 '>
-        <Link className="active:border-b-tomato" href={'/'}>
-          <img src='/Home.png' alt='Home' width='30' />
-        </Link>
+        <div className='flex items-center justify-center gap-6 md:flex sm:hidden'>
+          <Link className='active:border-b-tomato ' href={'/'}>
+            <img src='/Home.png' alt='Home' width='30' />
+          </Link>
 
-        <Link
-          className=' font-mina text-slate-900 hover:text-tomato active:text-tomato'
-          href={'/subreddits'}
+          <Link
+            className=' font-mina text-slate-900 hover:text-tomato active:text-tomato'
+            href={'/subreddits'}
+          >
+            Subreddits
+          </Link>
+          <Link
+            className=' font-mina text-slate-900 hover:text-tomato active:text-tomato'
+            href={'/logout'}
+          >
+            Logout
+          </Link>
+        </div>
+
+        <div className=' flex md:flex sm:hidden'>
+          <Link href='/login'>
+            <button className='flex justify-center items-center w-26 text-slate-900 font-mina text-sm leading-5 m-1 p-1 rounded-full shadow-md from-red-500 via-orange-400 to-yellow-400 bg-gradient-to-r hover:bg-radial-gradient  cursor-pointer'>
+              <span className='block text-black px-6 py-2 font-semibold rounded-full bg-white'>
+                Login
+              </span>
+            </button>
+          </Link>
+          <Link href='/register'>
+            <button className='flex justify-center items-center w-26 text-slate-900 font-mina text-sm leading-5 m-1 p-1 rounded-full shadow-md from-red-500 via-orange-400 to-yellow-400 bg-gradient-to-r hover:bg-radial-gradient  cursor-pointer'>
+              <span className='block text-black px-4 py-2 font-semibold rounded-full bg-white'>
+                Register
+              </span>
+            </button>
+          </Link>
+        </div>
+        <div
+          onClick={handleNav}
+          className='md:hidden lg:hidden xl:hidden 2xl:hidden cursor-pointer pl-24'
         >
-          Subreddits
-        </Link>
-        <Link
-          className=' font-mina text-slate-900 hover:text-tomato active:text-tomato'
-          href={'/logout'}
+          <AiOutlineMenu size={25} />
+        </div>
+        <div
+          className={
+            menuOpen
+              ? 'fixed right-0 top-24 w-[30%] mr-5 md:hidden shadow-lg rounded-md h-72 bg-[white] p-3 ease-in duration-500'
+              : 'fixed right-[-100%] top-24 p-3 ease-in duration-500'
+          }
         >
-          Logout
-        </Link>
-      </div>
+          <div className='flex justify-end '>
+            <div onClick={handleNav} className='cursor-pointer'>
+              <AiOutlineClose size={20} />
+            </div>
+          </div>
 
-      <div className=' flex gap-2 '>
-        <Link href='/login'>
-          <button className='flex justify-center items-center w-20 h-8 rounded-lg bg-light-blue-500 border-2 gradient-border text-slate-900 font-mina text-sm leading-5 no-underline hover:bg-radial-gradient cursor-pointer'>
-            Login
-          </button>
-        </Link>
-        <Link href='/register'>
-          <button className='flex justify-center items-center w-20 h-8 rounded-lg bg-light-blue-500 border-2 border-tomato text-slate-900 font-mina text-sm leading-5 no-underline hover:bg-radial-gradient cursor-pointer'>
-            Register
-          </button>
-        </Link>
-      </div>
-    </nav>
+          <div className='flex flex-col items-center justify-center text-center text-wrap p-0 gap-1'>
+            <div className='flex flex-col items-center mb-4 pb-1 gap-2'>
+              <Link className='active:border-b-tomato' href={'/'}>
+                <img src='/Home.png' alt='Home' width='30' />
+              </Link>
+              <br />
+              <Link
+                className=' font-mina text-slate-900 hover:text-tomato active:text-tomato'
+                href={'/subreddits'}
+              >
+                Subreddits
+              </Link>
+              <br />
+              <Link
+                className=' font-mina text-slate-900 hover:text-tomato active:text-tomato'
+                href={'/logout'}
+              >
+                Logout
+              </Link>
+            </div>
+
+            <Link href='/login'>
+              <button className='flex justify-center items-center w-26 text-slate-900 font-mina text-sm leading-5 m-1 p-1 rounded-full shadow-md from-red-500 via-orange-400 to-yellow-400 bg-gradient-to-r hover:bg-radial-gradient  cursor-pointer'>
+                <span className='block text-black px-6 py-2 font-semibold rounded-full bg-white'>
+                  Login
+                </span>
+              </button>
+            </Link>
+            <Link href='/register'>
+              <button className='flex justify-center items-center w-26 text-slate-900 font-mina text-sm leading-5 m-1 p-1 rounded-full shadow-md from-red-500 via-orange-400 to-yellow-400 bg-gradient-to-r hover:bg-radial-gradient  cursor-pointer'>
+                <span className='block text-black px-4 py-2 font-semibold rounded-full bg-white'>
+                  Register
+                </span>
+              </button>
+            </Link>
+          </div>
+        </div>
+      </nav>
+      {menuOpen && (
+        <div className='fixed inset-0 backdrop-blur-sm bg-black/20 z-10'></div>
+      )}
+    </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,22 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    screens: {
+      'sm': {'min': '400px', 'max': '767px'},
+      // => @media (min-width: 640px and max-width: 767px) { ... }
+
+      'md': {'min': '768px', 'max': '1023px'},
+      // => @media (min-width: 768px and max-width: 1023px) { ... }
+
+      'lg': {'min': '1024px', 'max': '1279px'},
+      // => @media (min-width: 1024px and max-width: 1279px) { ... }
+
+      'xl': {'min': '1280px', 'max': '1535px'},
+      // => @media (min-width: 1280px and max-width: 1535px) { ... }
+
+      '2xl': {'min': '1536px'},
+    },
+    
     fontFamily: {
       bungee: ['Bungee Spice', 'sans-serif'],
       mina: ['Mina', 'sans-serif'],
@@ -18,6 +34,7 @@ module.exports = {
       colors: {
         tomato: '#ff6347',
         redditblue: '#58c6fc',
+        orangy: '#f3c971',
       },
 
       backgroundImage: {
